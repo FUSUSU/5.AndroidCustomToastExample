657c174648fc504e0577ab32552f3d0d316720cc
c Thun Nguyn <nguyenducthuan.cit@gmail.com>
2017-01-05 20:35:45 +0700

    5. AndroidCustomToastExample

--- app/src/main/java/com/example/teo/a5androidcustomtoastexample/MainActivity.java
+++ app/src/main/java/com/example/teo/a5androidcustomtoastexample/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
@@ -16,17 +17,22 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Creating the LayoutInflater instance
+        //Creating the layoutInflater instance
         LayoutInflater li = getLayoutInflater();
+        //Getting the View Object as defined in the customtoast.xml file
+        View layout = li.inflate(R.layout.customtoast, (ViewGroup) findViewById(R.id.custom_toast_layout));
 
-        //Getting the View object as defined in the customtoast.xml file
-        View layout = li.inflate(R.layout.customtoast, (ViewGroup)findViewById(R.id.custom_toast_layout));
-
-        //Creating the Toast object
+        //Creating the Toast Object
         Toast toast = new Toast(getApplicationContext());
-        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setDuration(Toast.LENGTH_LONG);
         toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
-        toast.setView(layout); //setting the view of custom toast layout
+        toast.setView(layout); //Setting the view of custom toast layout
         toast.show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
     }
 }
